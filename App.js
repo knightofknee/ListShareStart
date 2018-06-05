1827e158af17c87c2239aca328221604a6dc9f00
knightofknee <bricarlis@gmail.com>
2018-06-04 20:33:04 -0500

    new branch, killed comments

--- App.js
+++ App.js
@@ -12,19 +12,9 @@ export default class App extends React.Component {
     this.ref = firebase.firestore().collection('items')
     this.state = {
       // firebase things?
-      loadingLogin: true
+      loading: true
     };
   }
-  // addItem() {
-  //   this.ref.add({
-  //     food: this.state.textInput,
-  //     status: false
-  //   })
-  //   this.setState({textInput: ''})
-  // }
-  // updateTextInput(value) {
-  //   this.setState({ textInput: value });
-  // }
   onLogin = () => {
     const { email, password } = this.state;
     firebase.auth().signInWithEmailAndPassword(email, password)
@@ -63,88 +53,18 @@ export default class App extends React.Component {
         user,
       })
     })
-    // this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
   }
   componentWillUnmount() {
     this.authSubscription()
   }
 
-//   onCollectionUpdate = (querySnapshot) => {
-//     let items = [];
-//     querySnapshot.forEach((doc) => {
-//       const { food, status } = doc.data();
-//       items.push({
-//         key: doc.id,
-//         doc, // DocumentSnapshot
-//         food,
-//         status,
-//       });
-//   });
-//   this.setState({
-//     items,
-//     loading: false,
-//  });
-//   }
-  // renderSeparator = () => {
-  //   return (
-  //     <View
-  //       style={{
-  //         height: 1,
-  //         width: "90%",
-  //         backgroundColor: "#CED0CE",
-  //         marginLeft: "5%"
-  //       }}
-  //     />
-    // );}
-
   render() {
-    // if (this.state.loadingLogin) return null
+    if (this.state.loading) return null
     if (this.state.user) return 'hii logged in'
     return <ListMain/>
-    return (
-      <ListMain/>
-    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    // alignItems: 'stretch',
-    // paddingLeft: 30
-  },
-  header: {
-    height: 20,
-    marginTop: 50,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
-  },
-  separator: {
-    flex: 1,
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#8E8E8E',
-  }
+
 });
